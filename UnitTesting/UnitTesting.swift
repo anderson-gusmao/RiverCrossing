40b5af73dad8050176dc9271bd066a3596b2cdea
Anderson Santos Gusmão <anderson.santos.gusmao@gmail.com>
2019-04-10 18:26:51 -0300

    Update .travis.yml (#3)
    * Update .travis.yml
    
    * Addung unit tests target
    
    * Changing to build locally without signing.
    
    * Removing unit tests
    
    * Update .travis.yml

---
+++ UnitTesting/UnitTesting.swift
@@ -0,0 +1,18 @@
+//
+//  UnitTesting.swift
+//  UnitTesting
+//
+//  Created by Anderson Gusmao on 10/04/19.
+//  Copyright © 2019 Anderson Santos Gusmão. All rights reserved.
+//
+
+import XCTest
+
+class UnitTesting: XCTestCase {
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+}
