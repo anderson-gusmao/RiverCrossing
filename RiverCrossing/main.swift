4832cd4045be67901620e35f8e4a0e9253baeb06
Anderson Santos Gusmão <anderson.santos.gusmao@gmail.com>
2019-04-10 20:44:13 -0300

    Development (#4)
    * Update .travis.yml
    
    * Addung unit tests target
    
    * Changing to build locally without signing.
    
    * Removing unit tests
    
    * Update .travis.yml
    
    * Simplifying logic.
    
    * Adding unit test.
    
    * Removing unused code.
    
    * Removing unused code.

--- RiverCrossing/main.swift
+++ RiverCrossing/main.swift
@@ -6,9 +6,7 @@
 //  Copyright © 2019 Anderson Santos Gusmão. All rights reserved.
 //
 
-
 let solution = Engine(initialState: State(rightSide: ["C","C","C","M", "M","M"],
                            leftSide: [],
                            direction: State.Direction.rightToLeft)).run()
 print(solution)
-
--- UnitTesting/UnitTesting.swift
+++ UnitTesting/UnitTesting.swift
@@ -15,4 +15,19 @@ class UnitTesting: XCTestCase {
 
     override func tearDown() {
     }
+
+    func testCheckSolution() {
+        let solution = Engine(initialState: State(rightSide: ["C","C","C","M", "M","M"],
+                                                  leftSide: [],
+                                                  direction: State.Direction.rightToLeft)).run()
+        XCTAssertEqual(solution, "[] <== [\"M\", \"M\", \"C\", \"C\", \"C\", \"M\"]\n[\"C\", \"M\"] ==> [\"M\", \"M\", \"C\", \"C\"]\n[\"C\"] <== [\"M\", \"M\", \"M\", \"C\", \"C\"]\n[\"C\", \"C\", \"C\"] ==> [\"M\", \"M\", \"M\"]\n[\"C\", \"C\"] <== [\"C\", \"M\", \"M\", \"M\"]\n[\"M\", \"C\", \"C\", \"M\"] ==> [\"C\", \"M\"]\n[\"M\", \"C\"] <== [\"C\", \"C\", \"M\", \"M\"]\n[\"M\", \"M\", \"M\", \"C\"] ==> [\"C\", \"C\"]\n[\"M\", \"M\", \"M\"] <== [\"C\", \"C\", \"C\"]\n[\"C\", \"M\", \"M\", \"M\", \"C\"] ==> [\"C\"]\n[\"C\", \"M\", \"M\", \"M\"] <== [\"C\", \"C\"]\n[\"C\", \"C\", \"C\", \"M\", \"M\", \"M\"] ==> []\n")
+    }
+    
+    func testMeasure() {
+        self.measure {
+            let _ = Engine(initialState: State(rightSide: ["C","C","C","M", "M","M"],
+                                                      leftSide: [],
+                                                      direction: State.Direction.rightToLeft)).run()
+        }
+    }
 }
